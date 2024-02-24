4cd30265e6a6f75a36aaccb33b2af9f6545c685a
Ahmed Ashraf <117744931+ahmedashraf792002@users.noreply.github.com>
2024-02-24 21:53:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
-from PIL import Image
-import io
 from keras.preprocessing import image
+import io
+from keras.applications.vgg16 import preprocess_input
 app = Flask(__name__)
 model = load_model('model_VGG16.h5')
 
-from keras.utils import img_to_array
-
 def preprocess_image(img_path):
-    op_img = Image.open(img_path)
-    img_resize = op_img.resize((224, 224))
-    img2arr = img_to_array(img_resize) / 255.0
-    img_reshape = img2arr.reshape(1, 224, 224, 3)
-    return img_reshape
+    # Preprocessing the image
+    img = image.load_img(img_path, target_size=(224,224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    return x
 
 @app.route('/')
 def index():
@@ -22,26 +22,23 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        if request.method == 'POST':
-            img = preprocess_image(request.files['file'].stream)
-            preds = model.predict(img)
-        
-            # Convert prediction probabilities to class labels
-            dic = {0: "Alzheimer's disease", 1: "Cognitively normal", 2: "Early mild cognitive impairment", 3: "Late mild cognitive impairment"}
-        
-            pred_class = dic[np.argmax(preds)]   # Get the class label with maximum probability
-            pred_proba = str(round(float(preds[0, np.argmax(preds)]),2))+"%"  # Get the probability of the predicted class
-            return  jsonify(pred_class+" "+pred_proba)
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-    except:
-        error = "File cannot be processed."
-        return jsonify(error)
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
 
+    image = file.read()
+    '''processed_image = preprocess_image(image)
+    preds = model.predict(processed_image)
 
+    # Convert prediction probabilities to class labels
+    dic = {0: "Alzheimer's disease", 1: "Cognitively normal", 2: "Early mild cognitive impairment", 3: "Late mild cognitive impairment"}
 
-
-
+    pred_class = dic[np.argmax(preds)]   # Get the class label with maximum probability
+    pred_proba = str(round(float(preds[0, np.argmax(preds)]),2))+"%"  # Get the probability of the predicted class'''
+    return  return jsonify({'Prediction':pred_class ,'Probability':pred_proba})
 
 if __name__ == '__main__':
     app.run(debug=True)
